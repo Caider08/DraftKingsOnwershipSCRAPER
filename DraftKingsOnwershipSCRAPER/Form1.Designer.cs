123284f4cac808f55db40e7d512f08fd02161f90
Caider08 <caidbuch@ksu.edu>
2017-09-27 00:31:49 -0500

    first commit

--- DraftKingsOnwershipSCRAPER/Form1.Designer.cs
+++ DraftKingsOnwershipSCRAPER/Form1.Designer.cs
@@ -1,6 +1,8 @@
-﻿namespace DraftKingsOnwershipSCRAPER
+﻿using System;
+
+namespace DraftKingsOnwershipSCRAPER
 {
-    partial class Form1
+    partial class GetPlayerTags
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +30,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.playerTagsGridView = new System.Windows.Forms.DataGridView();
+            this.SiteLogIn = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.navigate = new System.Windows.Forms.Button();
+            this.ScrapySharpTest = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.playerTagsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playerTagsGridView
+            // 
+            this.playerTagsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.playerTagsGridView.Location = new System.Drawing.Point(729, 12);
+            this.playerTagsGridView.Name = "playerTagsGridView";
+            this.playerTagsGridView.RowTemplate.Height = 24;
+            this.playerTagsGridView.Size = new System.Drawing.Size(596, 477);
+            this.playerTagsGridView.TabIndex = 0;
+            this.playerTagsGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // SiteLogIn
+            // 
+            this.SiteLogIn.Location = new System.Drawing.Point(12, 455);
+            this.SiteLogIn.Name = "SiteLogIn";
+            this.SiteLogIn.Size = new System.Drawing.Size(149, 70);
+            this.SiteLogIn.TabIndex = 1;
+            this.SiteLogIn.Text = "SiteLogIn";
+            this.SiteLogIn.UseVisualStyleBackColor = true;
+            this.SiteLogIn.Click += new System.EventHandler(this.SiteLogIn_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(689, 425);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // navigate
+            // 
+            this.navigate.Location = new System.Drawing.Point(190, 455);
+            this.navigate.Name = "navigate";
+            this.navigate.Size = new System.Drawing.Size(158, 65);
+            this.navigate.TabIndex = 3;
+            this.navigate.Text = "navigate";
+            this.navigate.UseVisualStyleBackColor = true;
+            this.navigate.Click += new System.EventHandler(this.navigate_Click);
+            // 
+            // ScrapySharpTest
+            // 
+            this.ScrapySharpTest.Location = new System.Drawing.Point(385, 459);
+            this.ScrapySharpTest.Name = "ScrapySharpTest";
+            this.ScrapySharpTest.Size = new System.Drawing.Size(145, 60);
+            this.ScrapySharpTest.TabIndex = 4;
+            this.ScrapySharpTest.Text = "ScrapySharpTest";
+            this.ScrapySharpTest.UseVisualStyleBackColor = true;
+       
+            // 
+            // GetPlayerTags
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 537);
+            this.Controls.Add(this.ScrapySharpTest);
+            this.Controls.Add(this.navigate);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.SiteLogIn);
+            this.Controls.Add(this.playerTagsGridView);
+            this.Name = "GetPlayerTags";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GetPlayerTags_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.playerTagsGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
+ 
+
         #endregion
+
+        private System.Windows.Forms.DataGridView playerTagsGridView;
+        private System.Windows.Forms.Button SiteLogIn;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button navigate;
+        private System.Windows.Forms.Button ScrapySharpTest;
     }
 }
 
--- DraftKingsOnwershipSCRAPER/Form1.cs
+++ DraftKingsOnwershipSCRAPER/Form1.cs
@@ -7,14 +7,162 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using ScrapySharp.Network;
+using ScrapySharp.Extensions;
 
 namespace DraftKingsOnwershipSCRAPER
 {
-    public partial class Form1 : Form
+    public partial class GetPlayerTags : Form
     {
-        public Form1()
+        DataTable table1;
+
+        public GetPlayerTags()
         {
             InitializeComponent();
+            
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void InitTable()
+        {
+            DataTable table1 = new DataTable("PlayerTagsDataTable");
+            table1.Columns.Add("Rank", typeof(int));
+            table1.Columns.Add("Name", typeof(string));
+            table1.Columns.Add("Tags", typeof(int));
+            table1.Columns.Add("Salary", typeof(int));
+            table1.Columns.Add("Start", typeof(int));
+            table1.Columns.Add("Sit", typeof(int));
+
+            playerTagsGridView.DataSource = table1;
+
+        }
+        private async void GetPlayerTags_Load(object sender, EventArgs e)
+        {
+            InitTable();
+            //var proxy = WebRequest.GetSystemWebProxy();
+            WebProxy proxy1 = new WebProxy();
+            proxy1.UseDefaultCredentials = false;
+
+            webBrowser1.Navigate("https://www.fansharesports.com/shared/signIn?redirectUrl=%2Fgolf%2Ftrends");
+
+           // System.Diagnostics.Process.Start("https://www.fansharesports.com/shared/signIn?redirectUrl=%2Fgolf%2Ftrends");
+
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument weeksTags = await Task.Factory.StartNew(() => web.Load("https://stackoverflow.com/questions/23298532/htmlagilitypack-and-authentication"));
+            var node = weeksTags.DocumentNode.SelectNodes("//*[@id=\"question\"]/table//tr[1]/td[2]/div/div[2]/a[1]");
+            var innerTexts = node.Select(x => x.InnerText);
+            //HtmlAgilityPack.HtmlDocument weeksTags = await Task.Factory.StartNew(() => web.Load("https://www.fansharesports.com/golf/analytics/tags", "GET",  proxy1, new NetworkCredential { UserName = "caiderwaider@hotmail.com", Password = "soccer08FS"}));
+           // var node = weeksTags.DocumentNode.SelectNodes("//*[@id=\"DataTables_Table_0\"]//tr[1]/td[2]/a");
+            
+
+        }
+
+        private void SiteLogIn_Click(object sender, EventArgs e)
+        {
+            var inputEmail = webBrowser1.Document.GetElementById("email");
+            inputEmail.Focus();
+            inputEmail.InnerText = "caiderwaider@hotmail.com";
+
+            var inputPassword = webBrowser1.Document.GetElementById("password");
+            inputPassword.Focus();
+            inputPassword.InnerText = "soccer08FS";
+
+            var submit = webBrowser1.Document.GetElementById("submit");
+            submit.InvokeMember("click");
+
+            System.Threading.Thread.Sleep(5);
+
+            var fafabar = webBrowser1.Document.GetElementById("hdr-mobile-menu");
+            fafabar.InvokeMember("click");
+
+
+           // var navbar = webBrowser1.Document.GetElementById("hdr-mobile-menu");
+
+          //  var links = navbar.GetElementsByTagName("a");
+
+          //  foreach (HtmlElement ele in links)
+           // {
+                // if (ele.GetAttribute("href").Equals("/golf/analytics/tags"))
+              //  if (ele.InnerText.Equals("FanShare Pro"))
+              //  {
+
+                  //  ele.InvokeMember("Click");
+               // }
+           // }
+
+            //var fanSharePro = webBrowser1.Document.GetElementById("hdr-nav");
+
+            //foreach (HtmlElement  in fanSharePro)
+            //{
+
+            // }
+
+            // var navbar = webBrowser1.Document.GetElementById("hdr-nav");
+            //var links = navbar.GetElementsByTagName("a");
+
+            // foreach (HtmlElement ele in links)
+            // {
+            // if (ele.GetAttribute("href").Equals("/golf/analytics/tags"))
+            // {
+
+            // ele.InvokeMember("Click");
+            // }
+            // }
+
+
+            //webBrowser1.Navigate("https://www.fansharesports.com/golf/analytics/tags");
+
+
+        }
+
+        private void navigate_Click(object sender, EventArgs e)
+        {
+
+            webBrowser1.Navigate("https://www.fansharesports.com/golf/analytics/tags");
+
+
+            //var navbar = webBrowser1.Document.GetElementById("#hdr-mobile-menu");
+           
+           // var links = navbar.GetElementsByTagName("a");
+
+           // foreach (HtmlElement ele in links)
+           // {
+               // if (ele.GetAttribute("href").Equals("/golf/analytics/tags"))
+             //  if (ele.InnerText.Equals("FanShare Pro"))
+               // {
+
+                 //   ele.InvokeMember("Click");
+                //}
+            //}
+         }
+
+        private void ScrapySharpTest_Click(object sender, EventArgs e)
+        {
+            ScrapingBrowser Browser = new ScrapingBrowser();
+            Browser.AllowAutoRedirect = true;
+            Browser.AllowMetaRedirect = true;
+
+            WebPage PageResult = Browser.NavigateToPage(new Uri("https://www.fansharesports.com/golf/trends"));
+            HtmlNode TestNode = PageResult.Html.CssSelect(".container").First();
+            string test = TestNode.InnerText;
+
+         
+            
+            
+
+           // webBrowser1.Navigate("https://www.fansharesports.com/golf/trends");
+            
+
+
+        }
+
+        
     }
 }
